74202e07c8849b8287391b6efb26024d2e2eb119
Hey <hey@MacBookAir.lan>
2026-09-15 12:08:38 +0530

    Update condition script

--- installation.sh
+++ installation.sh
@@ -9,19 +9,36 @@ then
     exit 1
 fi
 
-dnf list installed git
+# dnf list installed git
+
+# if [ $? -ne 0 ]
+# then
+#     echo "Git is not installed, going to install it.."
+#     dnf install git -y # here installing
+#     if [ $? -ne 0 ]
+#     then
+#         echo "Git installation is not success...check it"
+#         exit 1
+#     else
+#         echo "Git installation is success"
+#     fi
+# else
+#     echo "Git is already installed, nothing to do.."
+# fi
+
+dnf list installed mysql 
 
 if [ $? -ne 0 ]
 then
-    echo "Git is not installed, going to install it.."
-    dnf install git -y # here installing
+    echo "MySQL is not installed...going to install"
+    dnf install mysql -y
     if [ $? -ne 0 ]
     then
-        echo "Git installation is not success...check it"
+        echo "MySQL installation is failure..please check"
         exit 1
     else
-        echo "Git installation is success"
+        echo "MySQL installation is success"
     fi
 else
-    echo "Git is already installed, nothing to do.."
+    echo "MySQL is already installed..nothing to do"
 fi
